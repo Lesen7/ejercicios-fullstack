0955a96ac5d7c34893403310ffda023f50f6603b
Enzo Lespart <enzolespart@gmail.com>
2018-04-19 19:11:25 +0100

    estructura básica

---
+++ futbol-js/js/index.js
@@ -0,0 +1,9 @@
+var dom = 
+{
+  main: document.querySelector('#main'),
+  title: document.querySelector('#title'),
+  field: document.querySelector('#field'),
+  teamList: document.querySelector('#teamList'),
+  controlArea: document.querySelector('#controlArea')
+  
+}
