e11a63545b45b01acad113afe1815d4e12a18bab
Enzo Lespart <enzolespart@gmail.com>
2018-04-20 17:03:20 +0100

    página home y diseño consistente

---
+++ index.js
@@ -0,0 +1,13 @@
+// Objeto para las rutas de la página.
+var webPage =
+{
+  die: 'dado-js/',
+  bingo: 'bingo-js/',
+  football: 'futbol-js/'
+}
+
+// Enviar a la página especificada.
+function redirect(page)
+{
+  window.location = page;
+}
